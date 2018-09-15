3e04746cd780a39cd9d803f1ed85349a8348970e
ctw <ctw.3@163.com>
2018-09-15 17:25:38 +0800

    数据库结构设计，路径设置，安装vuedragger，建设Vue.js单文件组件app.vue，和packs/applications.js

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -7,4 +7,24 @@
 // To reference this file, add <%= javascript_pack_tag 'application' %> to the appropriate
 // layout file, like app/views/layouts/application.html.erb
 
-console.log('Hello World from Webpacker')
+
+// import TurbolinksAdapter from 'vue-turbolinks'
+import Vue from 'vue/dist/vue.esm'
+import App from '../app.vue'
+
+// Vue.use(TurbolinksAdapter)
+
+document.addEventListener('turbolinks:load', () => {
+  var element = document.getElementById("boards")
+  if ( element != undefined ) {
+    const app = new Vue({
+      el: element,
+      data: {
+        lists: JSON.parse(element.dataset.lists)
+      },
+      // 不理解template
+      template: "<App v-bind:original_lists='lists' />",
+      components: {App}
+    })
+  }
+})
--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,7 @@ Rails.application.routes.draw do
   resources :cards
   resources :lists
   devise_for :users
+
+  root to: 'lists#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
