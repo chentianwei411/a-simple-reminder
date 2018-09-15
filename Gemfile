abfa0c9eeb5ece1e7c571000dfe4852829beb82e
ctw <ctw.3@163.com>
2018-09-15 11:26:38 +0800

    add scaffold and add gem acts_as_list

--- Gemfile
+++ Gemfile
@@ -45,6 +45,7 @@ end
 gem 'devise'
 gem 'bootstrap', '~> 4.1.3'
 gem 'jquery-rails'
+gem 'acts_as_list'
 
 group :development do
   # Access an interactive console on exception pages or by calling 'console' anywhere in the code.
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,23 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_09_15_023009) do
+ActiveRecord::Schema.define(version: 2018_09_15_025238) do
+
+  create_table "cards", force: :cascade do |t|
+    t.integer "list_id"
+    t.string "name"
+    t.integer "position"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["list_id"], name: "index_cards_on_list_id"
+  end
+
+  create_table "lists", force: :cascade do |t|
+    t.string "name"
+    t.integer "position"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
