5aeaa1758e6751e32d8ee2320c8cf6cf42f8aa06
ctw <ctw.3@163.com>
2018-09-16 14:01:33 +0800

    change template and add method to add cards in app.vue。 use Rails.ajax()

--- app/javascript/app.vue
+++ app/javascript/app.vue
@@ -1,24 +1,66 @@
 <template>
   <div id="app" class='row'>
-    <div v-for='list in original_lists' class="col-3">
-      <h6>{{ list.name }}</h6>
-      <hr>
-      <div v-for="(card, index) in list.cards" class='card card-body'>
-        {{card.name}}
+      <div v-for='(list, index) in lists' class="col-3">
+        <h6 >{{ list.name }}</h6>
+        <hr>
+        <div class='card'>
+          <div v-for="(card, index) in list.cards" class='card-body mb-3'>
+            {{card.name}}.
+          </div>
+          <div class="card-footer">
+            <div class="input-group input-group-sm">
+              <input type="text" class="form-control" @input="messages[list.id] = $event.target.value" v-bind:value='messages[list.id]'>
+              <div class="input-group-append">
+                <span  @click="submitMessages(list.id)" class="input-group-text">add</span>
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
-    </div>
   </div>
 </template>
 
 <script>
 export default {
-  props: ["original_lists"]
+  props: ["original_lists",],
+  data: function() {
+    return {
+      messages:{},
+      lists: this.original_lists,
+    }
+  },
+  methods: {
+    submitMessages: function(list_id) {
+      var data = new FormData()
+      // card记录有list_id和name，2个字段属性。
+      data.append("card[list_id]", list_id)
+      data.append("card[name]", this.messages[list_id])
+
+      // 注意，传入的参数list_id, name都要通过参数白名单，和模型validats
+      // 手脚架自动生成的card_params方法中没有:list_id，所以自己要改，否则报告错误
+      // list: [must exits]即@list.errors
+      Rails.ajax({
+        url: '/cards',
+        type: "POST",
+        data: data,
+        dataType: 'json',
+        success: (data) => {
+          const index = this.lists.findIndex(item => item.id == list_id)
+          this.lists[index].cards.push(data)
+          this.messages[list_id] = undefined
+        }
+      })
+    }
+  }
 }
 </script>
 
 <style scoped>
+h6 {
+  text-align: center;
+}
 p {
-  font-size: 2em;
+  font-size: 4em;
   text-align: center;
 }
 </style>
--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -8,11 +8,12 @@
 // layout file, like app/views/layouts/application.html.erb
 
 
-// import TurbolinksAdapter from 'vue-turbolinks'
+import TurbolinksAdapter from 'vue-turbolinks'
 import Vue from 'vue/dist/vue.esm'
 import App from '../app.vue'
+import draggable from 'vuedraggable'
 
-// Vue.use(TurbolinksAdapter)
+Vue.use(TurbolinksAdapter)
 
 document.addEventListener('turbolinks:load', () => {
   var element = document.getElementById("boards")
@@ -24,7 +25,28 @@ document.addEventListener('turbolinks:load', () => {
       },
       // 不理解template
       template: "<App v-bind:original_lists='lists' />",
-      components: {App}
+      components: {App, draggable}
+    })
+  }
+
+  var main = document.getElementById("main")
+  if ( main != undefined) {
+    var vm = new Vue({
+      el: main,
+      data: {
+        list: [
+          {name: 'John'}, {name: "123"}, {name: "world!!!"}
+        ]
+      },
+      methods: {
+        add: function() {
+          this.list.push({name: "hahhas"})
+        },
+        replace: function() {
+          this.list= [{}]
+        }
+      },
+      components: { draggable }
     })
   }
 })
